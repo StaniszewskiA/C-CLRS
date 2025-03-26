48e66cde82cfc5341a0ae547eb29afb9321b3642
AStaniszewski <staniszewski01adam@gmail.com>
2025-03-26 13:58:12 +0100

    cd

--- src/part_4_advanced_design_and_analysis_techniques/15_greedy_algorithms/problems.c
+++ src/part_4_advanced_design_and_analysis_techniques/15_greedy_algorithms/problems.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
 
-#define TASK 1
+#define TASK 3
 
 /*
     15-1: Coin changing.
@@ -63,8 +64,106 @@ void make_change_optimal(int denoms[], int k, int n) {
 }
 
 /*
-    15-2: Scheduling to minize average completion time.
+    15-2 a: Scheduling to minize average completion time.
+
+    O(n*log(n))
 */
+typedef struct {
+    int id;
+    int pTime; // Abb. for processing time
+} Task;
+
+int compare(const void* a, const void* b) {
+    return ((Task*)a)->pTime - ((Task*)b)->pTime;
+}
+
+void schedule_tasks(Task tasks[], int n) {
+    qsort(tasks, n, sizeof(Task), compare);
+    
+    unsigned totalCompletionTime = 0;
+    int i;
+
+    for (i = 0; i < n; i++) totalCompletionTime += tasks[i].pTime;
+
+    double avgCompletionTime = (double) totalCompletionTime / n;
+    printf("Average Completion Time: %.2f\n", avgCompletionTime);
+}
+
+/*
+    15-2 b: Scheduling to minize average completion time of online tasks.
+
+    O(n^2) (?)
+*/
+typedef struct {
+    int id;
+    int pTime;
+    int rTime; // Abb. for release time
+    int remainingTime; // In case of stoppped execution
+} OnlineTask;
+
+int compare_online_p_times(const void* a, const void* b) {
+    return ((OnlineTask*)a)->pTime - ((OnlineTask*)b)->pTime;
+}
+
+int compare_online_r_times(const void* a, const void* b) {
+    return ((OnlineTask*)a)->rTime - ((OnlineTask*)b)->rTime;
+}
+
+int compare_online_remaining_times(const void* a, const void* b) {
+    return ((OnlineTask*)a)->remainingTime - ((OnlineTask*)b)->remainingTime;
+}
+
+void schedule_online_tasks(OnlineTask tasks[], int n) {
+    qsort(tasks, n, sizeof(OnlineTask), compare_online_r_times);
+
+    unsigned totalCompletionTime = 0;
+    unsigned completedTasks = 0;
+    unsigned currTime = 0;
+
+    OnlineTask* q = malloc(n * sizeof(OnlineTask));
+    int qSize = 0;
+    int i;
+
+    while (completedTasks < n) {
+        while (completedTasks < n && tasks[completedTasks].rTime <= currTime) {
+            q[qSize++] = tasks[completedTasks];
+            completedTasks++;
+        }
+
+        if (qSize > 0) {
+            qsort(q, qSize, sizeof(OnlineTask), compare_online_p_times);
+            OnlineTask currTask = q[0];
+
+            if (currTask.remainingTime == 0) {
+                currTask.remainingTime = currTask.pTime; 
+            }
+
+            unsigned timeSlice = 1;
+            currTask.remainingTime -= timeSlice;
+            currTime += timeSlice;
+
+            if (currTask.remainingTime > 0) {
+                q[0] = q[qSize - 1];
+                qSize--;
+                tasks[completedTasks - 1] = currTask;
+                qsort(q, qSize, sizeof(OnlineTask), compare_online_p_times);
+            } else {
+                totalCompletionTime += currTime;
+                for (i = 1; i < qSize; i++) q[i - 1] = q[1];
+                qSize--;
+            }
+
+        } else {
+            // No executable tasks.
+            currTime++;
+        }
+    }
+
+    double avgCompletionTime = (double) totalCompletionTime / n;
+    printf("Average Completion Time: %.2f\n", avgCompletionTime);
+
+    free(q);
+}
 
 int main(void) {
     switch (TASK)
@@ -75,16 +174,39 @@ int main(void) {
             // make_change_greedy(n);
 
             int denoms[] = {1, 2, 3};
-            int k = sizeof(denoms) / sizeof(denoms[0]);
-            int n = 6;
+            int k1 = sizeof(denoms) / sizeof(denoms[0]);
+            int n1 = 6;
 
-            make_change_optimal(denoms, k, n);
+            make_change_optimal(denoms, k1, n1);
 
             break;
         }
 
         case 2: {
-            // 15-2
+            // 15-2 a
+            Task tasks[] = {
+                {1, 3},  
+                {2, 5},  
+                {3, 2}
+            };
+            int n2 = sizeof(tasks) / sizeof(tasks[0]);
+
+            schedule_tasks(tasks, n2);
+
+            break;
+        }
+
+        case 3: {
+            // 15-2 b
+            OnlineTask tasks[] = {
+                {1, 6, 1}, 
+                {2, 5, 2},  
+                {3, 2, 1} 
+            };
+            int n3 = sizeof(tasks) / sizeof(tasks[0]);
+
+            schedule_online_tasks(tasks, n3);
+
             break;
         }
         
