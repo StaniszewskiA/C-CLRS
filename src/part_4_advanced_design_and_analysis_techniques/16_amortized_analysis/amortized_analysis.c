395d862715a9b0cea323f390b571b8ffb3bb8dce
AStaniszewski <staniszewski01adam@gmail.com>
2025-03-28 16:51:02 +0100

    The potential method

--- src/part_4_advanced_design_and_analysis_techniques/16_amortized_analysis/amortized_analysis.c
+++ src/part_4_advanced_design_and_analysis_techniques/16_amortized_analysis/amortized_analysis.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define EXAMPLE 2
+#define EXAMPLE 4
 
 /*
     Example 1 - Multipop Heap
@@ -75,6 +75,25 @@ int heap_pop(MinHeap* heap) {
 
     The accounting method - assuming a cost of 2 for push, 0 for pop, 
     and 0 for multipush—we can estimate the overall cost to be O(n).
+
+    The potential method - for an empty heap:
+        
+        Φ(D_i) >= 0
+                = Φ(D_0)
+
+    In case of a push operation:
+
+        Φ(D_i) - Φ(D_{i-1}) = (s + 1) - s = 1
+
+    which yields the amortized cost of: 1 + 1 = 2
+
+    In case of a multipop operation:
+
+        Φ(D_i) - Φ(D_{i-1}) = -k'
+
+    which yields the amortized cost of: k' - k' = 0
+
+    Thus the overall cost of n operations is O(n).
 */
 void heap_multipop(MinHeap* heap, int k) {
     int i;
@@ -125,7 +144,19 @@ void init_counter(BinaryCounter* bCounter) {
     amortized cost of 2 for this operation. Then, changing a bit to 0 can 
     be paid for using the accumulated amortization from setting bits to 1. 
     Since every bit is initially 0, the amortization balance never goes 
-    negative. This results in an overall cost of O(n)."
+    negative. This results in an overall cost of O(n).
+
+    The potential method - let's assume that the bCounter data structure's
+    potential upon increase is equals to the number of the "on" bits. Let's 
+    denote it as b_i. With each increase operation, the potential grows by:
+
+        Φ(D_i) - Φ(D_{i-1}) <= (b_{i - 1} - t_i + 1) - b_{i - 1}
+                            = 1 - t_i
+
+    This yields an amortized cost of 2.
+
+    If we execute at least n = Ω(k) increment operations, the overall
+    cost will be O(n), despite the initial value of the counter.
 */
 void increment_counter(BinaryCounter* bCounter) {
     int carry = 1;
@@ -162,7 +193,183 @@ void print_counter(BinaryCounter* bCounter) {
 }
 
 /*
-    Example 3 - Dynamic Tables
+    16.3-5 - amortized queue implemented with two stacks.
+*/
+#define MAX_STACK_SIZE 100
+
+typedef struct {
+    int data[MAX_STACK_SIZE];
+    int top;
+} Stack;
+
+void init_stack(Stack* s) {
+    s->top = -1;
+}
+
+int is_stack_empty(Stack* s) {
+    return s->top == -1;
+}
+
+void push_to_stack(Stack* s, int val) {
+    s->data[++(s->top)] = val;
+    printf("   push(%d) real cost = 1\n", val);
+}
+
+int pop_from_stack(Stack* s) {
+    int val = s->data[(s->top)--];
+    printf("   pop() -> %d real cost = 1\n", val);
+    return val;
+}
+
+/*
+    Enqueue yields an amortized cost of 3, and dequeue has a cost of 0. 
+    Without amortization, each operation costs 1. 
+    Enqueue generates credit for popping and pushing.
+*/
+
+typedef struct {
+    Stack s1;
+    Stack s2;
+    int credits;
+} Queue;
+
+void init_queue(Queue* q) {
+    init_stack(&q->s1);
+    init_stack(&q->s2);
+    q->credits = 0;
+}
+
+void enqueue(Queue* q, int val) {
+    printf("Enqueue(%d)\n", val);
+    q->credits += 2;
+    printf("   Assign amortized cost = 3, saved credits = %d\n", q->credits);
+    push_to_stack(&q->s1, val);
+}
+
+int dequeue(Queue* q) {
+    printf("Dequeue()\n");
+    if (is_stack_empty(&q->s2)) {
+        while (!is_stack_empty(&q->s1)) {
+            printf("   Move from s1 to s2 using 1 credit\n");
+            q->credits--;
+            int moved = pop_from_stack(&q->s1);
+            push_to_stack(&q->s2, moved);
+        }
+    }
+
+    if (is_stack_empty(&q->s2)) {
+        printf("   Queue is empty\n");
+        exit(1);
+    }
+
+    printf("   Pop from S2 using 1 credit\n");
+    q->credits--;
+    int result = pop_from_stack(&q->s2);
+    printf("   Remaining credits = %d\n", q->credits);
+
+    return result;
+}
+
+/*
+    16.3-6 - Dynamic Multiset
+*/
+#define INITIAL_DMS_CAPACITY 4
+
+typedef struct {
+    int* data;
+    unsigned size;
+    unsigned capacity;
+} DynamicMultiset;
+
+DynamicMultiset* create_dms() {
+    DynamicMultiset* dms = malloc(sizeof(DynamicMultiset));
+    dms->data = malloc(INITIAL_DMS_CAPACITY);
+    dms->size = 0;
+    dms->capacity = INITIAL_DMS_CAPACITY;
+
+    return dms;
+}
+
+void free_dms(DynamicMultiset* dms) {
+    free(dms->data);
+    free(dms);
+}
+
+void resize_dms(DynamicMultiset* dms, int newCapacity) {
+    dms->data = realloc(dms->data, newCapacity * sizeof(int));
+    dms->capacity = newCapacity;
+}
+
+void insert_dms(DynamicMultiset* dms, int val) {
+    if (dms->size == dms->capacity) resize_dms(dms, dms->capacity * 2);
+    dms->data[dms->size++] = val;
+}
+
+int parition(int* arr, int left, int right, int pivotIdx) {
+    int i;
+    
+    int pivotVal = arr[pivotIdx];
+    swap(&arr[pivotIdx], &arr[right]);
+    int storeIdx = left;
+
+    for (i = left; i < right; i++) {
+        if (arr[i] < pivotVal) {
+            swap(&arr[storeIdx], &arr[i]);
+            storeIdx++;
+        }
+    }
+    swap(&arr[right], &arr[storeIdx]);
+    return storeIdx;
+}
+
+int quickselect(int* arr, int left, int right, int k) {
+    if (left == right) return arr[left];
+
+    int pivotIdx = left + rand() % (right - left + 1);
+    pivotIdx = parition(arr, left, right, pivotIdx);
+
+    int count = pivotIdx - left + 1;
+
+    if (k == count) return arr[pivotIdx];
+    else if (k < count) return quickselect(arr, left, pivotIdx - 1, k);
+    else return quickselect(arr, pivotIdx + 1, right, k - count);
+}
+
+void delete_larger_half(DynamicMultiset* dms) {
+    if (dms->size == 0) return;
+
+    int i, j;
+
+    int k = (dms->size + 1) / 2;
+    int* temp = malloc(dms->size * sizeof(int));
+
+    // Copy
+    for (i = 0; i < dms->size; i++) temp[i] = dms->data[i];
+    
+    int threshold = quickselect(temp, 0, dms->size - 1, k);
+    free(temp);
+    
+    j = 0;
+
+    for (i = 0; i < dms->size; i++) {
+        if (dms->data[i] <= threshold) dms->data[j++] = dms->data[i];
+    }
+
+    dms->size = j;
+
+    if (dms->size <= dms->capacity / 4 && dms->capacity > INITIAL_DMS_CAPACITY)
+        resize_dms(dms, dms->capacity / 2);
+}
+
+void print_dms(DynamicMultiset* dms) {
+    int i;
+    printf("Multiset: ");
+    for(i = 0; i < dms->size; i++) printf("%d, ", dms->data[i]);
+    printf("\n");
+}
+
+/*
+    16.4 - Dynamic Tables
 */
 
 int main(void) {
@@ -209,7 +416,43 @@ int main(void) {
         }
 
         case 3: {
-            // Dynamic Tables
+            // 16.3-5
+            Queue q;
+            init_queue(&q);
+
+            enqueue(&q, 10);
+            enqueue(&q, 20);
+            enqueue(&q, 30);
+
+            printf("Dequeued: %d\n\n", dequeue(&q));
+            printf("Dequeued: %d\n\n", dequeue(&q));
+
+            enqueue(&q, 40);
+            printf("Dequeued: %d\n\n", dequeue(&q));
+            printf("Dequeued: %d\n\n", dequeue(&q));
+
+            break;
+        }
+
+        case 4: {
+            // 16.3-6 - Dynamic Multiset
+            DynamicMultiset* dms = create_dms();
+
+            insert_dms(dms, 5);
+            insert_dms(dms, 2);
+            insert_dms(dms, 8);
+            insert_dms(dms, 1);
+            insert_dms(dms, 10);
+            insert_dms(dms, 7);
+
+            print_dms(dms);
+
+            delete_larger_half(dms);
+
+            print_dms(dms);
+
+            free_dms(dms);
+
             break;
         }
 
