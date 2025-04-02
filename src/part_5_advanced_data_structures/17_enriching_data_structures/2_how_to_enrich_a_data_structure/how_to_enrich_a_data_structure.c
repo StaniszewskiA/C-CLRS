cfde82895215bd8453b62a3e4e386813d6513bfc
AStaniszewski <staniszewski01adam@gmail.com>
2025-04-02 14:08:39 +0200

    How to enrich data structures

--- src/part_5_advanced_data_structures/17_enriching_data_structures/2_how_to_enrich_a_data_structure/how_to_enrich_a_data_structure.c
+++ src/part_5_advanced_data_structures/17_enriching_data_structures/2_how_to_enrich_a_data_structure/how_to_enrich_a_data_structure.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define TASK 1
+#define TASK 2
 
 #define RED 0
 #define BLACK 1
@@ -12,6 +13,8 @@ typedef struct RBNode {
     int color;
     uint key;
     uint size;
+    uint blackHeight;
+    int depth;
     struct RBNode* left;
     struct RBNode* right;
     struct RBNode* parent;
@@ -26,17 +29,34 @@ typedef struct RBTree {
     RBNode* max;
 } RBTree;
 
+RBNode* create_rbnode(RBTree* T, int key);
+RBTree* create_rbtree();
+void left_rotate(RBTree* T, RBNode* x);
+void right_rotate(RBTree* T, RBNode* y);
+void rb_insert_fixup(RBTree* T, RBNode* z);
+void rb_insert(RBTree* T, int key);
+void inorder_traversal(RBNode* root, RBNode* nil);
+RBNode* find_min(RBTree* T);
+RBNode* find_max(RBTree* T);
+RBNode* find_successor(RBNode* x);
+RBNode* find_predecessor(RBNode* x);
+void update_black_height(RBNode* x);
+void update_depth(RBNode* x);
+
+
 RBNode* create_rbnode(RBTree* T, int key) {
     RBNode* node = (RBNode*)malloc(sizeof(RBNode));
     node->key = key;
     node->color = RED;
     node->size = 1;
+    node->blackHeight = 0;
+    node->depth = 0;
     node->left = T->nil;
     node->right = T->nil;
     node->parent = T->nil;
-    node->successor = T->nil;  
-    node->predecessor = T->nil;  
-    printf("Successfuly created a RBNode with a key of %d\n", key);
+    node->successor = T->nil;
+    node->predecessor = T->nil;
+    printf("Successfully created a RBNode with a key of %d\n", key);
     return node;
 }
 
@@ -45,10 +65,12 @@ RBTree* create_rbtree() {
     T->nil = (RBNode*)malloc(sizeof(RBNode));
     T->nil->color = BLACK;
     T->nil->size = 0;
+    T->nil->blackHeight = 0;
+    T->nil->depth = -1;
     T->root = T->nil;
     T->min = T->nil;
     T->max = T->nil;
-    printf("Successfuly created the RBTree\n");
+    printf("Successfully created the RBTree\n");
     return T;
 }
 
@@ -67,12 +89,16 @@ void left_rotate(RBTree* T, RBNode* x) {
 
     y->size = x->size;
     x->size = x->left->size + x->right->size + 1;
+
+    update_depth(x);
+    update_depth(y);
+    update_black_height(x);
+    update_black_height(y);
 }
 
 void right_rotate(RBTree* T, RBNode* y) {
     RBNode* x = y->left;
     y->left = x->right;
-
     if (y->left != T->nil) y->left->parent = y;
 
     x->parent = y->parent;
@@ -86,12 +112,18 @@ void right_rotate(RBTree* T, RBNode* y) {
 
     x->size = y->size;
     y->size = y->left->size + y->right->size + 1;
+
+    update_depth(y);
+    update_depth(x);
+    update_black_height(y);
+    update_black_height(x);
 }
 
 void rb_insert_fixup(RBTree *T, RBNode *z) {
     while (z->parent->color == RED) {
+        RBNode *y;
         if (z->parent == z->parent->parent->left) {
-            RBNode *y = z->parent->parent->right;
+            y = z->parent->parent->right;
             if (y->color == RED) {  
                 z->parent->color = BLACK;
                 y->color = BLACK;
@@ -107,7 +139,7 @@ void rb_insert_fixup(RBTree *T, RBNode *z) {
                 right_rotate(T, z->parent->parent);
             }
         } else {
-            RBNode *y = z->parent->parent->left;
+            y = z->parent->parent->left;
             if (y->color == RED) {
                 z->parent->color = BLACK;
                 y->color = BLACK;
@@ -125,6 +157,13 @@ void rb_insert_fixup(RBTree *T, RBNode *z) {
         }
     }
     T->root->color = BLACK;
+
+    RBNode* node = z;
+    while (node != T->nil) {
+        update_black_height(node);
+        update_depth(node);
+        node = node->parent;
+    }
 }
 
 void rb_insert(RBTree* T, int key) {
@@ -134,7 +173,7 @@ void rb_insert(RBTree* T, int key) {
 
     while (x != T->nil) {
         y = x;
-        x->size++; 
+        x->size++;
         if (z->key < x->key) x = x->left;
         else x = x->right;
     }
@@ -153,33 +192,13 @@ void rb_insert(RBTree* T, int key) {
     if (T->min == T->nil || z->key < T->min->key) T->min = z;
     if (T->max == T->nil || z->key > T->max->key) T->max = z;
 
-    RBNode* temp;
-
-    // Successor
-    if (z->right != T->nil) {
-        temp = z->right;
-        while (temp->left != T->nil) temp = temp->left;
-    } else {
-        temp = z->parent;
-        while (temp != T->nil && z == temp->right) {
-            z = temp;
-            temp = temp->parent;
-        }
-    }
+    // Set successor and predecessor
+    RBNode* temp = z->right != T->nil ? z->right : z->parent;
+    while (temp != T->nil && z == temp->right) temp = temp->parent;
     z->successor = temp;
 
-    // Predecessor
-    if (z->left != T->nil) {
-        temp = z->left;
-        while (temp->right != T->nil) temp = temp->right;
-    } else {
-        temp = z->parent;
-        while (temp != T->nil && z == temp->left) {
-            z = temp;
-            temp = temp->parent;
-        }
-    }
-
+    temp = z->left != T->nil ? z->left : z->parent;
+    while (temp != T->nil && z == temp->left) temp = temp->parent;
     z->predecessor = temp;
 
     rb_insert_fixup(T, z);
@@ -195,6 +214,7 @@ void inorder_traversal(RBNode* root, RBNode* nil) {
 /*
     17.2-1
 */
+
 RBNode* find_min(RBTree* T) {
     return T->min;
 }
@@ -211,6 +231,21 @@ RBNode* find_predecessor(RBNode* x) {
     return x->predecessor;
 }
 
+/*
+    17.2-2
+*/
+void update_black_height(RBNode* x) {
+    while (x && x != x->parent) {
+        int leftBlackHeight = x->left ? x->left->blackHeight : 0;
+        int rightBlackHeight = x->right ? x->right->blackHeight : 0;
+        x->blackHeight = 1 + (leftBlackHeight > rightBlackHeight ? leftBlackHeight : rightBlackHeight);
+        x = x->parent;
+    }
+}
+
+void update_depth(RBNode* x) {
+    if (x) x->depth = x->parent ? x->parent->depth + 1 : 0;
+}
 
 int main(void) {
     RBTree* T = create_rbtree();
@@ -226,10 +261,8 @@ int main(void) {
     printf("Inorder traversal with sizes:\n");
     inorder_traversal(T->root, T->nil);
 
-    switch (TASK)
-    {
+    switch (TASK) {
         case 1: {
-            // 17.2-1
             RBNode* minNode = find_min(T);
             printf("Minimum key node: %d\n", minNode->key);
 
@@ -243,11 +276,19 @@ int main(void) {
             node = T->root;
             RBNode* predecessor = find_predecessor(node);
             printf("Predecessor of node with key %d is %d\n", node->key, predecessor->key);
+            break;
+        }
+        case 2: {
+            // 17.2-2 functionality (to be implemented)
+            RBNode* x = find_max(T);
+            printf("Black height of the max node: %d\n", x->blackHeight);
+            printf("Depth of the max node: %d\n", x->depth);
 
             break;
         }
         default:
             break;
-        }
+    }
+
     return 0;
 }
