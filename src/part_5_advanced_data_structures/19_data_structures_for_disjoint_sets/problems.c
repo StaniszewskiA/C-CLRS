e8bbf3a689fe3ce7702d7527ae0cc92ce9246b8d
AStaniszewski <staniszewski01adam@gmail.com>
2025-04-16 12:20:10 +0200

    Depth determination

--- src/part_5_advanced_data_structures/19_data_structures_for_disjoint_sets/problems.c
+++ src/part_5_advanced_data_structures/19_data_structures_for_disjoint_sets/problems.c
@@ -2,30 +2,29 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define TASK 1
+#define TASK 2
+
+#pragma region Off-line minimum
+
 #define MAX_N 1000
 
-typedef struct DisjointSet {
+typedef struct OfflineDisjointSet {
     int parent;
     int rank;
     int minVal;
     int nextSet;
     int exists;
-} DisjointSet;
+} OfflineDisjointSet;
 
-/*
-    19.1: Off-line minimum, O(nα(n))
-*/
-
-DisjointSet sets[MAX_N];
+OfflineDisjointSet sets[MAX_N];
 int extracted[MAX_N];
 
-void make_set(int i);
-int find_set(int i);
-void union_sets(int x, int y);
+void offline_make_set(int i);
+int offline_find_set(int i);
+void offline_union_sets(int x, int y);
 void offline_minimum(int ops[], int n);
 
-void make_set(int i) {
+void offline_make_set(int i) {
     sets[i].parent = i;
     sets[i].rank = 0;
     sets[i].minVal = INT_MAX;
@@ -33,14 +32,14 @@ void make_set(int i) {
     sets[i].exists = 1; 
 }
 
-int find_set(int i) {
-    if (sets[i].parent != i) sets[i].parent = find_set(sets[i].parent);
+int offline_find_set(int i) {
+    if (sets[i].parent != i) sets[i].parent = offline_find_set(sets[i].parent);
     return sets[i].parent;
 }
 
-void union_sets(int x, int y) {
-    int xRoot = find_set(x);
-    int yRoot = find_set(y);
+void offline_union_sets(int x, int y) {
+    int xRoot = offline_find_set(x);
+    int yRoot = offline_find_set(y);
     if (xRoot == yRoot) return;
 
     if (sets[xRoot].rank < sets[yRoot].rank) {
@@ -61,7 +60,7 @@ void offline_minimum(int ops[], int n) {
     int i;
 
     for (i = 0; i < n; i++) {
-        if (ops[i] == -1) make_set(extractIdx++);
+        if (ops[i] == -1) offline_make_set(extractIdx++);
     }
 
     int currSet = 0;
@@ -71,7 +70,7 @@ void offline_minimum(int ops[], int n) {
         if (ops[i] == -1) currSet++;
         else {
             int x = ops[i];
-            int rep = find_set(currSet);
+            int rep = offline_find_set(currSet);
             if (x < sets[rep].minVal) sets[rep].minVal = x;
         }
     }
@@ -79,14 +78,14 @@ void offline_minimum(int ops[], int n) {
     for (i = 0; i < currSet; i++) {
         if (!sets[i].exists) continue;
 
-        int rep = find_set(i);
+        int rep = offline_find_set(i);
         extracted[i] = sets[rep].minVal;
 
         int next = sets[i].nextSet;
         while (next < currSet && !sets[next].exists) next++;
 
         if (next < currSet) {
-            union_sets(i, next);
+            offline_union_sets(i, next);
             sets[i].nextSet = next + 1;
         }
 
@@ -100,6 +99,68 @@ void offline_minimum(int ops[], int n) {
     printf("\n");
 }
 
+#pragma endregion Off-line minimum
+
+#pragma region Depth determination
+
+typedef struct DeterminantNode {
+    struct DeterminantNode* parent;
+    int rank;
+    int pseudoDepth;
+} DeterminantNode;
+
+DeterminantNode* determinant_make_tree();
+DeterminantNode* determinant_find_set(DeterminantNode* v, int* depth);
+int determinant_find_depth(DeterminantNode* v);
+void determinant_graft(DeterminantNode* r, DeterminantNode* v);
+
+DeterminantNode* determinant_make_tree() {
+    DeterminantNode* obj = malloc(sizeof(DeterminantNode));
+    obj->parent = obj;
+    obj->rank = 0;
+    obj->pseudoDepth = 0;
+    return obj;
+}
+
+DeterminantNode* determinant_find_set(DeterminantNode* v, int* depth) {
+    if (v != v->parent) {
+        int parentDepth;
+        DeterminantNode* root = determinant_find_set(v->parent, &parentDepth);
+        v->pseudoDepth += parentDepth;
+        v->parent = root;
+        *depth = v->pseudoDepth;
+        return root;
+    } else {
+        *depth = 0;
+        return v;
+    }
+}
+
+int determinant_find_depth(DeterminantNode* v) {
+    int depth;
+    determinant_find_set(v, &depth);
+    return depth;
+}
+
+void determinant_graft(DeterminantNode* r, DeterminantNode* v) {
+    int depthR, depthV;
+    DeterminantNode* rootR = determinant_find_set(r, &depthR);
+    DeterminantNode* rootV = determinant_find_set(v, &depthV);
+
+    if (rootR == rootV) return;
+
+    if (rootR->rank > rootV->rank) {
+        rootV->parent = rootR;
+        rootV->pseudoDepth = depthR - depthV;
+    } else {
+        rootR->parent = rootV;
+        rootR->pseudoDepth = depthV - depthR + 1;
+        if (rootR->rank == rootV->rank) rootV->rank += 1;
+    }
+}
+
+#pragma endregion Depth determination
+
 int main(void) {
     switch (TASK)
     {
@@ -109,6 +170,31 @@ int main(void) {
             int n = sizeof(ops) / sizeof(ops[0]);
 
             offline_minimum(ops, n);
+            break;
+        }
+        case 2:{
+            // 19.2
+            DeterminantNode* a = determinant_make_tree();
+            DeterminantNode* b = determinant_make_tree();
+            DeterminantNode* c = determinant_make_tree();
+
+            printf("Initial depths:\n");
+            printf("depth(a) = %d\n", determinant_find_depth(a));
+            printf("depth(b) = %d\n", determinant_find_depth(b));
+            printf("depth(c) = %d\n", determinant_find_depth(c));
+
+            determinant_graft(b, a);
+            determinant_graft(c, b);
+
+            printf("\nAfter grafting:\n");
+            printf("depth(a) = %d\n", determinant_find_depth(a));
+            printf("depth(b) = %d\n", determinant_find_depth(b));
+            printf("depth(c) = %d\n", determinant_find_depth(c));
+
+            free(a);
+            free(b);
+            free(c);
+
             break;
         }
         default:
