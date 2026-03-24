da840fa750f71f0cb0419bec514d5d207f7d16d9
AStaniszewski <staniszewski01adam@gmail.com>
2026-03-24 11:36:15 +0100

    Chapter 33 init

---
+++ src/part_7_selected_topics/33_machine_learning_algorithms/1_clustering/main.c
@@ -0,0 +1,16 @@
+#include "part_7_selected_topics/33_machine_learning_algorithms/33_machine_learning_algorithms.h"
+
+#define TASK 1
+
+int main(void) {
+    switch (TASK)
+    {
+        case 1: {
+            printf("TODO");
+            break;
+        }
+
+        default:
+            break;
+    }
+}
